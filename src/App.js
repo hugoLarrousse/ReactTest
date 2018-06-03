b03bec545a9796db7017c281dc1d6b813ade48ad
Hugo Larrousse <hugo.larrousse@gmail.com>
2018-06-03 07:38:40 +0200

    hall of fame fake + using map to create element for the game (memory)

--- src/App.js
+++ src/App.js
@@ -1,46 +1,88 @@
 import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
 import './App.css'
 import Card from './Card'
-import { cards } from './Card'
+// import { cards } from './Card'
 import GuessCount from './GuessCount'
+import HallOfFame, { FAKE_HOF } from './HallOfFame'
 
-console.log(cards)
-
-
-const Greeter = ({ whom }) => (
-  <button onClick={() => console.log(`Bonjour ${whom} !`)}>
-    Vas-y, clique !
-  </button>
-)
-
-const Win = () => (
- true && <p> Gagné </p>
-)
-
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 
 class App extends Component {
+  cards = this.generateCards()
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    console.log(candidates);
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
   handleCardClick(card) {
     console.log(card, 'clicked')
   }
+
   render() {
+    const won = true;
     return (
-      <div>
       <div className="memory">
         <GuessCount guesses={0} />
-        {cards.map((card, index) => (
+        {this.cards.map((card,index) => (
           <Card
             card={card}
-            feedback="visible"
-            key={index}
+            feedback='hidden'
             onClick={this.handleCardClick}
+            key={index}
           />
         ))}
-      </div>
-      <Greeter whom= "Jean"/>
-      <Win />
+        {won && <HallOfFame entries={FAKE_HOF} />}
       </div>
     )
   }
 }
+
+
+// const Greeter = ({ whom }) => (
+//   <button onClick={() => console.log(`Bonjour ${whom} !`)}>
+//     Vas-y, clique !
+//   </button>
+// )
+
+// const Win = () => (
+//  true && <p> Gagné </p>
+// )
+
+
+// class App extends Component {
+//   handleCardClick(card) {
+//     console.log(card, 'clicked')
+//   }
+//   render() {
+//     return (
+//       <div>
+//       <div className="memory">
+//         <GuessCount guesses={0} />
+//         {cards.map((card, index) => (
+//           <Card
+//             card={card}
+//             feedback="visible"
+//             key={index}
+//             onClick={this.handleCardClick}
+//           />
+//         ))}
+//       </div>
+//       <Greeter whom= "Jean"/>
+//       <Win />
+//       </div>
+//     )
+//   }
+// }
 
 export default App
